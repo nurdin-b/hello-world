49c37cbc13048020171395d29ae3ab2cca758c02
nuradeen <nuradeen.bing0899@gmail.com>
2023-09-08 17:09:04 +0700

    Initial commit

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,17 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PostsController {
+    public async index(ctx: HttpContextContract) {
+        return [
+          {
+            id: 1,
+            title: 'Hello world',
+          },
+          {
+            id: 2,
+            title: 'Hello universe',
+          },
+        ]
+    }
+}
